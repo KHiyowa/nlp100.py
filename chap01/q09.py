148c03682490512ba5d7cf8e92e00e5c88fbfc7b
Hiyowa Kyobashi <kh.iyowa@gmail.com>
2015-11-18 11:15:22 +0900

    Solved Q09

--- chap01/q09.py
+++ chap01/q09.py
@@ -1,8 +1,23 @@
+import random
+
 def shuffle(text):
     list = text.replace(".", "").replace(":", "").split()
     align_t = []
     align_n = []
     shuffle = []
 
+    for i in range(len(list)):
+        if i == 0 or i == len(list)-1 or len(list[0]) <= 4:
+            align_n.append(i)
+            align_t.append(list.pop(0))
+        else:
+            shuffle.append(list.pop(0))
 
+    random.shuffle(shuffle)
+
+    for i in align_n:
+        shuffle.insert(i, align_t.pop(0))
+    return shuffle
+
+print(shuffle(input()))
 
