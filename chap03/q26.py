405ca670a84fe27ac449611733386db25ec0239b
Hiyowa Kyobashi <kh.iyowa@gmail.com>
2015-11-30 02:38:40 +0900

    作業中

---
+++ chap03/q26.py
@@ -0,0 +1,22 @@
+import re
+
+result = []
+
+f = open("jawiki-uk.txt", "r")
+string = f.read()
+f.close()
+
+data = str(re.findall("{{基礎情報.+\n}}", string, re.DOTALL)).split(r"\n")
+
+dic = {}
+
+for datum in data:
+    if re.search("\|.+=.+", datum) != None:
+        tmpkey = re.sub("\|", "", datum)
+        tmpkey = re.sub(" =.+", "", tmpkey)
+        tmpvalue = re.sub(".+=", "", datum)
+        tmpvalue = re.sub("\'\'+", "", tmpvalue)
+        dic[tmpkey] = tmpvalue
+
+print(dic)
+print(len(dic))
